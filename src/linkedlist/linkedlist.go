4862773aae7a4b826f96dfd29426a8412ea1bd1c
root <>
2026-06-03 23:24:00 -0700

    container: add list, set, queue, stack, heaps, tree, linkedlist
    Generic std packages for growable lists, sets, FIFO/LIFO, min/max heaps,
    BST, and doubly-linked lists with tests via test/containers.go.

---
+++ src/linkedlist/linkedlist.go
@@ -0,0 +1,98 @@
+// Copyright 2026 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package linkedlist provides a generic doubly-linked list.
+package linkedlist
+
+type element[T any] struct {
+	val        T
+	prev, next *element[T]
+}
+
+// List is a doubly-linked list.
+type List[T any] struct {
+	head, tail *element[T]
+	length     int
+}
+
+func New[T any]() *List[T] { return &List[T]{} }
+
+func FromSlice[T any](s []T) *List[T] {
+	l := New[T]()
+	for _, v := range s {
+		l.Append(v)
+	}
+	return l
+}
+
+func (l *List[T]) Len() int { return l.length }
+
+func (l *List[T]) Append(vals ...T) {
+	for _, v := range vals {
+		e := &element[T]{val: v}
+		if l.tail == nil {
+			l.head, l.tail = e, e
+		} else {
+			e.prev = l.tail
+			l.tail.next = e
+			l.tail = e
+		}
+		l.length++
+	}
+}
+
+func (l *List[T]) Prepend(v T) {
+	e := &element[T]{val: v}
+	if l.head == nil {
+		l.head, l.tail = e, e
+	} else {
+		e.next = l.head
+		l.head.prev = e
+		l.head = e
+	}
+	l.length++
+}
+
+func (l *List[T]) PopFront() (T, bool) {
+	if l.head == nil {
+		var z T
+		return z, false
+	}
+	v := l.head.val
+	l.head = l.head.next
+	if l.head == nil {
+		l.tail = nil
+	} else {
+		l.head.prev = nil
+	}
+	l.length--
+	return v, true
+}
+
+func (l *List[T]) PopBack() (T, bool) {
+	if l.tail == nil {
+		var z T
+		return z, false
+	}
+	v := l.tail.val
+	l.tail = l.tail.prev
+	if l.tail == nil {
+		l.head = nil
+	} else {
+		l.tail.next = nil
+	}
+	l.length--
+	return v, true
+}
+
+func (l *List[T]) ToSlice() []T {
+	out := make([]T, 0, l.length)
+	for e := l.head; e != nil; e = e.next {
+		out = append(out, e.val)
+	}
+	return out
+}
+
+// Values returns elements from head to tail.
+func (l *List[T]) Values() []T { return l.ToSlice() }
---
+++ src/maxheap/maxheap.go
@@ -0,0 +1,72 @@
+// Copyright 2026 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package maxheap
+
+import "cmp"
+
+type Heap[T cmp.Ordered] struct {
+	data []T
+}
+
+func New[T cmp.Ordered]() *Heap[T] { return &Heap[T]{} }
+
+func (h *Heap[T]) Len() int { return len(h.data) }
+
+func (h *Heap[T]) Push(v T) {
+	h.data = append(h.data, v)
+	h.up(len(h.data) - 1)
+}
+
+func (h *Heap[T]) Pop() (T, bool) {
+	if len(h.data) == 0 {
+		var z T
+		return z, false
+	}
+	v := h.data[0]
+	last := len(h.data) - 1
+	h.data[0] = h.data[last]
+	h.data = h.data[:last]
+	if len(h.data) > 0 {
+		h.down(0)
+	}
+	return v, true
+}
+
+func (h *Heap[T]) Peek() (T, bool) {
+	if len(h.data) == 0 {
+		var z T
+		return z, false
+	}
+	return h.data[0], true
+}
+
+func (h *Heap[T]) up(i int) {
+	for i > 0 {
+		p := (i - 1) / 2
+		if h.data[p] >= h.data[i] {
+			break
+		}
+		h.data[p], h.data[i] = h.data[i], h.data[p]
+		i = p
+	}
+}
+
+func (h *Heap[T]) down(i int) {
+	n := len(h.data)
+	for {
+		l, r, s := 2*i+1, 2*i+2, i
+		if l < n && h.data[l] > h.data[s] {
+			s = l
+		}
+		if r < n && h.data[r] > h.data[s] {
+			s = r
+		}
+		if s == i {
+			return
+		}
+		h.data[i], h.data[s] = h.data[s], h.data[i]
+		i = s
+	}
+}
