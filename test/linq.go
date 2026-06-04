af82762ba42ac5ee97a28455dbed441d33c1e6ec
root <>
2026-06-03 22:48:00 -0700

    linq: add generic lazy LINQ query package
    Provide FromSlice/Lazy* pipelines and eager Where, Select, Sum, OrderBy,
    and related helpers. Package-level functions only (no generic methods) to
    avoid compiler ICE on this toolchain.

---
+++ test/linq.go
@@ -0,0 +1,20 @@
+// run
+
+// Copyright 2026 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import "linq"
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5}
+	pipe := linq.LazySelect(linq.LazyWhere(linq.FromSlice(nums),
+		func(n int) bool { return n%2 == 0 }),
+		func(n int) int { return n * 2 })
+	first, ok := linq.LazyFirst(pipe)
+	if !ok || first != 4 {
+		panic(first)
+	}
+}
