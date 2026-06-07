5e6723a7b72384f6b7f8fef9a03209cf0a51e377
root <>
2026-06-06 23:30:00 -0700

    doc: add language features and FormatCustom time formatting specs.
    Update new_features index links and standardize copyright headers on fork-added source files.

--- test/result_assign_err.go
+++ test/result_assign_err.go
@@ -1,6 +1,6 @@
 // errorcheck
 
-// Copyright 2026 The Go Authors. All rights reserved.
+// Copyright authors of this Go fork
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
