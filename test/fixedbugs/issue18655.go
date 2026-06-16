39564d340fc696ad1f67747b8a7aef87db78fb9b
root <>
2026-06-15 21:45:00 -0700

    Update errorcheck patterns for method-overload duplicate diagnostics.
    Accept 'method T.m already declared' wording in alias2.go and issue18655.go.

--- test/fixedbugs/issue18655.go
+++ test/fixedbugs/issue18655.go
@@ -11,12 +11,12 @@ type A = T
 type B = T
 
 func (T) m() {}
-func (T) m() {} // ERROR "already declared|redefinition"
-func (A) m() {} // ERROR "already declared|redefinition"
-func (A) m() {} // ERROR "already declared|redefinition"
-func (B) m() {} // ERROR "already declared|redefinition"
-func (B) m() {} // ERROR "already declared|redefinition"
+func (T) m() {} // ERROR "method T\.m already declared|already declared|redefinition"
+func (A) m() {} // ERROR "method T\.m already declared|already declared|redefinition"
+func (A) m() {} // ERROR "method T\.m already declared|already declared|redefinition"
+func (B) m() {} // ERROR "method T\.m already declared|already declared|redefinition"
+func (B) m() {} // ERROR "method T\.m already declared|already declared|redefinition"
 
-func (*T) m() {} // ERROR "already declared|redefinition"
-func (*A) m() {} // ERROR "already declared|redefinition"
-func (*B) m() {} // ERROR "already declared|redefinition"
+func (*T) m() {} // ERROR "method T\.m already declared|already declared|redefinition"
+func (*A) m() {} // ERROR "method T\.m already declared|already declared|redefinition"
+func (*B) m() {} // ERROR "method T\.m already declared|already declared|redefinition"
