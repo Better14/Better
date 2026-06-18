d5e27c8defad73fd4a80e27b181e88166f6443e6
root <>
2026-06-17 19:03:31 -0700

    Fix gofmt printing doc comments on shorthand struct declarations.
    Use setComment for StructDecl and InterfaceDecl so go fix can format rewritten sources.

--- src/go/printer/nodes.go
+++ src/go/printer/nodes.go
@@ -2186,10 +2186,7 @@ func (p *printer) enumDecl(d *ast.EnumDecl) {
 }
 
 func (p *printer) structDecl(d *ast.StructDecl) {
-	if d.Doc != nil {
-		p.setPos(d.Doc.Pos())
-		p.print(d.Doc)
-	}
+	p.setComment(d.Doc)
 	p.setPos(d.Struct)
 	p.print(token.STRUCT, blank)
 	p.print(d.Name)
@@ -2200,10 +2197,7 @@ func (p *printer) structDecl(d *ast.StructDecl) {
 }
 
 func (p *printer) interfaceDecl(d *ast.InterfaceDecl) {
-	if d.Doc != nil {
-		p.setPos(d.Doc.Pos())
-		p.print(d.Doc)
-	}
+	p.setComment(d.Doc)
 	p.setPos(d.Interface)
 	p.print(token.INTERFACE, blank)
 	p.print(d.Name)
