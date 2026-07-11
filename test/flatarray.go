be190d14712c2ccfd71d99aafb3799796b9d8a84
root <>
2026-07-11 05:02:52 -0700

    Add fork copyright headers to Bow-added Go source files.
    Align root-added files with the project convention (Copyright authors of
    this Go fork). Modified upstream files keep the original Go Authors
    header; no per-file modification notices are required under BSD.

--- test/flatarray.go
+++ test/flatarray.go
@@ -1,3 +1,7 @@
+// Copyright authors of this Go fork
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // compile
 
 package main
--- test/nilable_ptr_arg.go
+++ test/nilable_ptr_arg.go
@@ -1,5 +1,9 @@
 // run
 
+// Copyright authors of this Go fork
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 func take(p *string?) {}
